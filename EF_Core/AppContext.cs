b33de8dc211a4dd9ed7e17acec89812dbf834c08
Роман Толмачёв <unifiq@gmail.com>
2021-05-28 12:18:00 +0700

    добавление AppContext

---
+++ EF_Core/AppContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EF_Core
+{
+	public class AppContext: DbContext
+	{
+		/// <summary>
+		/// Объект таблицы Users
+		/// </summary>
+		public DbSet<User> Users { get; set; }
+
+		public AppContext()
+		{
+			Database.EnsureCreated();
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			// параметр - строка подключения
+			optionsBuilder.UseSqlServer(@"Data Source=(local);Database=EF;Trusted_Connection=True;");
+		}
+	}
+}
--- EF_Core/Program.cs
+++ EF_Core/Program.cs
@@ -6,7 +6,19 @@ namespace EF_Core
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			// После создания контекста приложения можно использовать EF в проекте. 
+			// Для этого следует создать объект контекста с помощью конструктора без параметров.
+			// Поскольку DbContext реализует интерфейс IDisposable, мы можем использовать конструкцию using. Обернём создание контекста в using:
+			using (var db = new AppContext())
+			{
+				// Поскольку ORM позволяет работать с БД через объекты, то создадим несколько экземпляров созданного нами класса User:
+				var user1 = new User { Name = "Arthur", Role = "admin" };
+				var user2 = new User { Name = "Klim", Role = "User" };
+				// Далее добавим пользователей в таблицу Users, обращаясь к полю Users нашего контекста db, и сохраним изменения (об этом поговорим позже):
+				db.Users.Add(user1);
+				db.Users.Add(user2);
+				db.SaveChanges();
+			}
 		}
 	}
 }
